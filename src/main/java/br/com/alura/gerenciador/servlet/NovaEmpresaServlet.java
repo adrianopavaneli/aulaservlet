1fde008688acf180436280b3609386af2f65fe7b
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-09-13 21:22:15 -0300

    metodos post e listando empresas

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -22,6 +22,11 @@ public class NovaEmpresaServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa!");
 		String nomeEmpresa = request.getParameter("nome");
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
