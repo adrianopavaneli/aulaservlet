3509bb58ee28afb37d310df03eaf8dee62c91d83
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-09-18 20:37:22 -0300

    Melhorando modelos

--- src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -12,15 +12,15 @@ import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
 public class ListaEmpresas {
-	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Acao Listando empresa");
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 
 		request.setAttribute("empresas", lista);
+		return "forward:listaEmpresas.jsp";
 
-		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
-		rd.forward(request, response);
+		
 	}
 
 }
