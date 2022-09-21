9efb35641225e79d603ebdcfb95ada29ba9d585f
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-09-21 12:31:49 -0300

    Verifica usuario esta logado

--- src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
@@ -17,8 +18,18 @@ public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		String paramAcao = request.getParameter("acao");
+		
+		HttpSession sessao = request.getSession();
+		boolean usuarioNaoLogado = (sessao.getAttribute("usuariologado") == null);
+		boolean acaoProtegida = !(paramAcao.equals("LoginForm") || paramAcao.equals("Login")) ;
+		if(acaoProtegida && usuarioNaoLogado) {			
+			response.sendRedirect("entrada?acao=LoginForm");
+			return;
+		}
+		
+		
+		
 		
 		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
 		
